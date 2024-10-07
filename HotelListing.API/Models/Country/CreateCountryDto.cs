3f9ce4de1da0379ab62f9cce895de400770dbe10
qiuxan <qiuxan@gmail.com>
2024-10-07 19:35:53 +1100

    Refactor POST Method with Data Transfer Object

---
+++ HotelListing.API/Models/Country/CreateCountryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Country;
+
+public class CreateCountryDto
+{
+    [Required]
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+}
