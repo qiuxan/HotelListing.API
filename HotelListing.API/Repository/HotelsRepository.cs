4270db08d037274b4682792d02f1e5c2deaa7f27
qiuxan <qiuxan@gmail.com>
2024-10-18 08:31:17 +1100

    Add IHotelsRebpository, HotelsRepository and regiser DI in the program

---
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -0,0 +1,11 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Repository;
+
+public class HotelsRepository : GenericRepository<Hotel>, IHotelsRebpository
+{
+    public HotelsRepository(HotelListingDbContext context) : base(context)
+    {
+    }
+}
