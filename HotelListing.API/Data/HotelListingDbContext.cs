c033d33f789a0a15dd576c1900660b8f2606031d
qiuxan <qiuxan@gmail.com>
2024-10-23 09:05:31 +1100

    Add Default User Roles using config and refactoring hotel and country seeding files

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelListing.API.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Data;
@@ -17,53 +18,12 @@ public class HotelListingDbContext : IdentityDbContext<ApiUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Country>().HasData(
-            new Country
-            {
-                Id = 1,
-                Name = "Jamaica",
-                ShortName = "JM"
-            },
-            new Country
-            {
-                Id = 2,
-                Name = "Bahamas",
-                ShortName = "BS"
-            },
-            new Country
-            {
-                Id = 3,
-                Name = "Cayman Islands",
-                ShortName = "CI"
-            }
-        );
+        
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
 
-        modelBuilder.Entity<Hotel>().HasData(
-            new Hotel
-            {
-                Id = 1,
-                Name = "Sandals Resort and Spa",
-                Address = "Negril",
-                CountryId = 1,
-                Rating = 4.5
-            },
-            new Hotel
-            {
-                Id = 2,
-                Name = "Grand Palladium",
-                Address = "Nassau",
-                CountryId = 2,
-                Rating = 4
-            },
-            new Hotel
-            {
-                Id = 3,
-                Name = "Comfort Suites",
-                Address = "Seven Mile Beach",
-                CountryId = 3,
-                Rating = 4.3
-            }
-        );
+        modelBuilder.ApplyConfiguration(new CountryConfiguration());
+
+        modelBuilder.ApplyConfiguration(new HotelConfiguration());
 
  
     }
---
+++ HotelListing.API/Migrations/20241022220319_addDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "08de8919-6874-414f-af1c-a84cf7b32cb6", null, "User", "USER" },
+                    { "a220391b-c0f6-4eae-a5de-6dc6c36927cf", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "08de8919-6874-414f-af1c-a84cf7b32cb6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a220391b-c0f6-4eae-a5de-6dc6c36927cf");
+        }
+    }
+}
